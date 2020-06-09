6aaf0bc8c9f4da86455587abda9faba1bc8ca63f
Vinicius D Cerutti <51954708+viniciusdc@users.noreply.github.com>
2020-06-08 23:32:39 -0300

    Update `main`

--- migrate_update_version.py
+++ migrate_update_version.py
@@ -1,6 +1,13 @@
-from conda_forge_tick.utils import load_graph
-from conda_forge_tick.update_upstream_versions import (
-    get_latest_version,
+import networkx as nx
+import logging
+import random
+import json
+import os
+import tqdm
+
+from conda_forge_tick.utils import setup_logger, load_graph
+from conda_forge_tick.update_sources import (
+    AbstractSource,
     PyPI,
     CRAN,
     NPM,
@@ -8,49 +15,134 @@ from conda_forge_tick.update_upstream_versions import (
     RawURL,
     Github,
 )
-import random
-import json
+from typing import (
+    Any,
+    Optional,
+    Iterable,
+    Set,
+    Iterator,
+    List,
+)
 
-sources = (PyPI(), CRAN(), NPM(), ROSDistro(), RawURL(), Github())
+# conda_forge_tick :: cft
+logger = logging.getLogger("conda-forge-tick._update_versions")
 
-# Reading graph
-gx = load_graph()
-_all_nodes = [t for t in gx.nodes.items()]
-random.shuffle(_all_nodes)
 
-# Inspection the graph object and node update:
-# print(f"Number of nodes: {len(gx.nodes)}")
-Node_count = 0
+def get_latest_version(
+    name: str, payload_meta_yaml: Any, sources: Iterable[AbstractSource]
+):
+    with payload_meta_yaml as meta_yaml:
+        for source in sources:
+            logger.debug("source: %s", source.__class__.__name__)
+            url = source.get_url(meta_yaml)
+            logger.debug("url: %s", url)
+            if url is None:
+                continue
+            ver = source.get_version(url)
+            logger.debug("ver: %s", ver)
+            if ver:
+                return ver
+            else:
+                logger.debug(f"Upstream: Could not find version on {source.name}")
+                meta["bad"] = f"Upstream: Could not find version on {source.name}"
+        if not meta_yaml.get("bad"):
+            logger.debug("Upstream: unknown source")
+            meta["bad"] = "Upstream: unknown source"
+        return False
 
-to_update = {}
-to_update["nodes"] = []
-for node, node_attrs in _all_nodes:
-    # checking each node
-    with node_attrs["payload"] as attrs:
-        # verify the actual situation of the package;
-        actual_ver = str(attrs.get("version"))
+
+# It's expected that your environment provide this info.
+CONDA_FORGE_TICK_DEBUG = os.environ.get("CONDA_FORGE_TICK_DEBUG", False)
+
+
+def new_update_upstream_versions(
+    gx: nx.DiGraph, sources: Iterable[AbstractSource] = None
+) -> None:
+    sources = (
+        (PyPI(), CRAN(), NPM(), ROSDistro(), RawURL(), Github())
+        if sources is None
+        else sources
+    )
+
+    _all_nodes = [t for t in gx.nodes.items()]
+    random.shuffle(_all_nodes)
+
+    # Inspection the graph object and node update:
+    # print(f"Number of nodes: {len(gx.nodes)}")
+    Node_count = 0
+
+    to_update = []
+    for node, node_attrs in tqdm.tqdm(_all_nodes):
+        attrs = node_attrs["payload"]
         if attrs.get("bad") or attrs.get("archived"):
-            print(
-                f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - bad/archived"
-            )
-            Node_count += 1
+            attrs["new_version"] = False
             continue
-        # New verison request
-        try:
-            new_version = get_latest_version(node, attrs, sources)
-        except Exception as e:
-            try:
-                se = repr(e)
-            except Exception as ee:
-                se = "Bad exception string: {}".format(ee)
-                print(f"Warning: Error getting upstream version of {node}: {se}")
-        else:
-            print(
-                f"# {Node_count:<5} - {node:<30} - ver: {attrs.get('version'):<10} - new ver: {new_version}"
-            )
-        to_update["nodes"].append({"id": str(node), "version": str(new_version)})
-    Node_count += 1
+        to_update.append((node, attrs))
 
-# Writing  out file
-with open("new_version.json", "w") as outfile:
-    json.dump(to_update, outfile)
+    up_to = {}
+    for node, node_attrs in to_update:
+        # checking each node
+        with node_attrs as attrs:
+            up_to = {}
+
+            # verify the actual situation of the package;
+            actual_ver = str(attrs.get("version"))
+
+            # rude exception
+            if node == "ca-policy-lcg":
+                up_to["ca-policy-lcg"] = {
+                    "bad": attrs.get("bad"),
+                    "new_version": False,
+                    "new_version_attempts": attrs.get("new_version_attempts"),
+                    "new_version_errors": attrs.get("new_version_errors")
+                }
+                Node_count += 1
+                continue
+
+            # New verison request
+            try:
+                new_version = get_latest_version(node, attrs, sources)
+                attrs["new_version"] = new_version or attrs["new_version"]
+            except Exception as e:
+                try:
+                    se = repr(e)
+                except Exception as ee:
+                    se = "Bad exception string: {}".format(ee)
+                logger.warning(
+                    f"Warning: Error getting upstream version of {node}: {se}"
+                )
+                attrs["bad"] = "Upstream: Error getting upstream version"
+            else:
+                logger.info(
+                    f"# {Node_count:<5} - {node:<30} - ver: {attrs.get('version'):<10} - new ver: {attrs.get('new_version')}",
+                )
+            up_to[f"{node}"] = {
+                "bad": attrs.get("bad"),
+                "new_version": attrs.get("new_version"),
+                "new_version_attempts": attrs.get("new_version_attempts"),
+                "new_version_errors": attrs.get("new_version_errors")
+            }
+            Node_count += 1
+    return up_to
+
+
+def main(args: Any = None) -> None:
+    if CONDA_FORGE_TICK_DEBUG:
+        setup_logger(logger, level="debug")
+    else:
+        setup_logger(logger)
+
+    logger.info("Reading graph")
+    # Graph enabled for inspection
+    gx = load_graph()
+
+    # call update
+    to_update = new_update_upstream_versions(gx)
+
+    logger.info("writing out file")
+    with open("new_version.json", "w") as outfile:
+        json.dump(to_update, outfile)
+
+
+if __name__ == "__main__":
+    main()
