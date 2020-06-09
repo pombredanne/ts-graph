c68fd10f0cdc1d25ca8f0e90564a438e9fcf4187
Vinicius D Cerutti <51954708+viniciusdc@users.noreply.github.com>
2020-06-09 01:41:51 -0300

    Update update_sources.py

--- conda_forge_tick/update_sources.py
+++ conda_forge_tick/update_sources.py
@@ -19,8 +19,8 @@ import requests
 from conda.models.version import VersionOrder
 
 sys.path.append("..")
-from conda_forge_tick.utils import parse_meta_yaml
-from conda_forge_tick.hashing import hash_url
+from utils import parse_meta_yaml
+from hashing import hash_url
 
 # TODO: parse_version has bad type annotations
 from pkg_resources import parse_version
