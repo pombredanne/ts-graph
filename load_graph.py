4558bba449c23b38c06aa9ef08844a2659a16496
Vinicius D Cerutti <51954708+viniciusdc@users.noreply.github.com>
2020-06-09 01:55:14 -0300

    Update load_graph.py

--- load_graph.py
+++ load_graph.py
@@ -3,9 +3,11 @@ import logging
 import random
 import json
 import os
-import sys
+import tqdm
 
+import sys
 sys.path.append("..")
+
 from conda_forge_tick.utils import setup_logger, load_graph
 from conda_forge_tick.update_sources import (
     AbstractSource,
@@ -26,7 +28,7 @@ from typing import (
 )
 
 # conda_forge_tick :: cft
-logger = logging.getLogger("cft._update_versions")
+logger = logging.getLogger("conda-forge-tick._update_versions")
 
 
 def get_latest_version(
@@ -45,14 +47,20 @@ def get_latest_version(
                 return ver
             else:
                 logger.debug(f"Upstream: Could not find version on {source.name}")
+                meta["bad"] = f"Upstream: Could not find version on {source.name}"
         if not meta_yaml.get("bad"):
             logger.debug("Upstream: unknown source")
+            meta["bad"] = "Upstream: unknown source"
         return False
+
 
 # It's expected that your environment provide this info.
 CONDA_FORGE_TICK_DEBUG = os.environ.get("CONDA_FORGE_TICK_DEBUG", False)
 
-def new_update_upstream_versions(gx: nx.DiGraph, sources: Iterable[AbstractSource] = None) -> None:
+
+def new_update_upstream_versions(
+    gx: nx.DiGraph, sources: Iterable[AbstractSource] = None
+) -> None:
     sources = (
         (PyPI(), CRAN(), NPM(), ROSDistro(), RawURL(), Github())
         if sources is None
@@ -66,47 +74,62 @@ def new_update_upstream_versions(gx: nx.DiGraph, sources: Iterable[AbstractSourc
     # print(f"Number of nodes: {len(gx.nodes)}")
     Node_count = 0
 
-    to_update = {}
-    to_update["nodes"] = []
-    for node, node_attrs in _all_nodes:
+    to_update = []
+    for node, node_attrs in tqdm.tqdm(_all_nodes):
+        attrs = node_attrs["payload"]
+        if attrs.get("bad") or attrs.get("archived"):
+            attrs["new_version"] = False
+            continue
+        to_update.append((node, attrs))
+
+    up_to = {}
+    for node, node_attrs in to_update:
         # checking each node
-        with node_attrs["payload"] as attrs:
-            # rude exception
-            if node == "ca-policy-lcg":
-                to_update["nodes"].append({"id": str(node), "version": "False"})
-                Node_count += 1
-                continue
-            
+        with node_attrs as attrs:
+            up_to = {}
+
             # verify the actual situation of the package;
             actual_ver = str(attrs.get("version"))
-            if attrs.get("bad") or attrs.get("archived"):
-                logger.info(f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - bad/archived"
-                )
+
+            # rude exception
+            if node == "ca-policy-lcg":
+                up_to["ca-policy-lcg"] = {
+                    "bad": attrs.get("bad"),
+                    "new_version": False,
+                    "new_version_attempts": attrs.get("new_version_attempts"),
+                    "new_version_errors": attrs.get("new_version_errors")
+                }
                 Node_count += 1
                 continue
+
             # New verison request
             try:
                 new_version = get_latest_version(node, attrs, sources)
+                attrs["new_version"] = new_version or attrs["new_version"]
             except Exception as e:
                 try:
                     se = repr(e)
                 except Exception as ee:
-                    se = f"Bad exception string: {ee}"
-                logger.error(f"# {Node_count:<5} - Error getting upstream version of {node}: {se}")
-            else:
-                try:
-                    logger.info(f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - new ver: {new_version}"
+                    se = "Bad exception string: {}".format(ee)
+                logger.warning(
+                    f"Warning: Error getting upstream version of {node}: {se}"
                 )
-                except TypeError as expt:
-                    logger.warning(f"Warning: {expt} on {node}")
-                logger.info(f"# {Node_count:<5} - {node:<30} - ver: {actual_ver:<10} - new ver: {new_version}")
-            to_update["nodes"].append({"id": str(node), "version": str(new_version)})
+                attrs["bad"] = "Upstream: Error getting upstream version"
+            else:
+                logger.info(
+                    f"# {Node_count:<5} - {node:<30} - ver: {attrs.get('version'):<10} - new ver: {attrs.get('new_version')}",
+                )
+            up_to[f"{node}"] = {
+                "bad": attrs.get("bad"),
+                "new_version": attrs.get("new_version"),
+                "new_version_attempts": attrs.get("new_version_attempts"),
+                "new_version_errors": attrs.get("new_version_errors")
+            }
             Node_count += 1
-    return to_update
+    return up_to
 
 
 def main(args: Any = None) -> None:
-    logger.info("cft :: conda_forge_tick")
     if CONDA_FORGE_TICK_DEBUG:
         setup_logger(logger, level="debug")
     else:
@@ -126,5 +149,3 @@ def main(args: Any = None) -> None:
 
 if __name__ == "__main__":
     main()
-
-
